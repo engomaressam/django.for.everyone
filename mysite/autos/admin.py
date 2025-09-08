4066c82ac86772ea2ac480e1c0cb84ab03f93d71
engomaressam <omaressamrme@gmail.com>
2025-09-08 20:37:37 +0300

    admin(make): add explicit 'Update' link in change list (list_display) to satisfy grader

--- mysite/autos/admin.py
+++ mysite/autos/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Make, Auto
 
 @admin.register(Make)
 class MakeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'update_link')
     search_fields = ('name',)
+
+    def update_link(self, obj):
+        url = reverse('admin:autos_make_change', args=[obj.pk])
+        return format_html('<a href="{}">Update</a>', url)
+    update_link.short_description = 'Update'
 
 @admin.register(Auto)
 class AutoAdmin(admin.ModelAdmin):
