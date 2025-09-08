1b4e5a8893c7c3b288b9b85248ed74053a0aeebf
engomaressam <omaressamrme@gmail.com>
2025-09-08 20:43:25 +0300

    setup_user: grant add/change/view permissions for Make and Auto to avoid 403 on admin auto add

--- mysite/autos/management/commands/setup_user.py
+++ mysite/autos/management/commands/setup_user.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from autos.models import Make, Auto
 
 class Command(BaseCommand):
     help = 'Setup dj4e_user with staff privileges'
@@ -24,6 +27,20 @@ class Command(BaseCommand):
             user.save()
             
         user.set_password(password)
+        user.save()
+
+        # Ensure needed permissions on Make and Auto
+        make_ct = ContentType.objects.get_for_model(Make)
+        auto_ct = ContentType.objects.get_for_model(Auto)
+        needed_codenames = [
+            'view_make','add_make','change_make',
+            'view_auto','add_auto','change_auto'
+        ]
+        perms = list(Permission.objects.filter(
+            content_type__in=[make_ct, auto_ct],
+            codename__in=needed_codenames
+        ))
+        user.user_permissions.add(*perms)
         user.save()
         
         self.stdout.write(
