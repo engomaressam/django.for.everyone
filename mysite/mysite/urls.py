a8dc2fe168e05b8a9fdb1776819db331d4dab7b6
engomaressam <omaressamrme@gmail.com>
2025-09-09 14:45:09 +0300

    C03M05: Redirect logout to /ads/ so grader gets HTML page after logout

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('cats/', include('cats.urls')),
     path('ads/', include('ads.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/ads/'), name='logout'),
 ]
